68391452f02564e54af3d97668656aa30015546e
f41ardu <f41ardu@arcor.de>
2018-12-02 13:57:17 +0100

    Work on wget and loop

--- prepare.sh
+++ prepare.sh
@@ -15,8 +15,6 @@ fi
 for loop  in $(cat $1) 
 do 
 	
-#echo $loop 
-#wget -q -N -P lists/ad $loop
 list_item=$(echo $loop | sed 's/\//;/g' | cut -d ';' -f3)
 echo $list_item
 
@@ -24,5 +22,9 @@ echo $list_item
 if [ ! -d "lists/"$list_item ]; then 
      mkdir lists/$list_item
 fi
-wget -q -N -P lists/$list_item/ $loop
+wget -o -q -N -P lists/$list_item/ $loop
+
+# egrep -v '^\s*$|^#|localhost' psh.txt | awk {'print $2'} 
+
+done
 done
