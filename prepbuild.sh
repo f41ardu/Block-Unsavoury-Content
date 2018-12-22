4dc081130e4d5ff858c71940f5e9387b8ccf6730
f41ardu <f41ardu@arcor.de>
2018-12-22 17:09:35 +0100

    Add progress bar to wget

--- prepbuild.sh
+++ prepbuild.sh
@@ -52,15 +52,14 @@ do
 list_item=$(echo $loop | sed 's/\//;/g' | cut -d ';' -f3)
 #echo $list_item
 
-
 if [ ! -d "lists/"$list_item ]; then 
      mkdir lists/$list_item
 fi
 if [ ! -d "lists/$list_item/bak" ]; then
      mkdir lists/$list_item/bak
 fi
- echo "Download: " $list_item"--/--"$loop
- wget -T 4 -q -N -P lists/$list_item/ $loop
+#  echo "Download: " $list_item"--/--"$loop
+ wget -T 4 -q -N --show-progress -P  lists/$list_item/ $loop
 
 done
 
